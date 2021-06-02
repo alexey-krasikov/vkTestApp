3348d4c68ee4e85873528c0ab19f2be6cfe28983
Alexey <krasikov11@gmail.com>
2021-06-02 23:15:36 +0300

    zooming and pitching added

--- VkTestApp/ParametersTableViewController.swift
+++ VkTestApp/ParametersTableViewController.swift
@@ -16,10 +16,19 @@ class ParametersTableViewController: UITableViewController {
     
     var humans: [Human] = []
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    
+    @IBSegueAction func simulationSegueAction(_ coder: NSCoder, sender: Any?) -> SimulationViewController? {
+        let groupSize = Int(groupSizeTextField.text!) ?? 0
+        let infectionFactor = Int(infectionFactorTextField.text!) ?? 0
+        let recalculationPeriod = Int(recalculationPeriodTextField.text!) ?? 0
+        return SimulationViewController(coder: coder, groupSize: groupSize, infectionFactor: infectionFactor, recalculationPeriod: recalculationPeriod)
+    }
+    
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
---
+++ VkTestApp/SimulationViewController.swift
@@ -0,0 +1,85 @@
+//
+//  SimulationViewController.swift
+//  VkTestApp
+//
+//  Created by Алексей Красиков on 02.06.2021.
+//
+
+import UIKit
+
+class SimulationViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var simulationView: UIView!
+    
+    var groupSize: Int = Int()
+    var infectionFactor: Int = Int()
+    var recalculationPeriod: Int = Int()
+    var humans: [Human] = [Human]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.delegate = self
+        
+        createHumans()
+        drawHumans()
+        
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return simulationView
+    }
+    
+    func updateZoomFoor(size: CGSize) {
+        let widthScale = size.width / simulationView.bounds.width
+        let heightScale = size.height / simulationView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFoor(size: view.bounds.size)
+    }
+    
+    init?(coder: NSCoder, groupSize: Int, infectionFactor: Int, recalculationPeriod: Int)
+    {
+        self.groupSize = groupSize
+        self.infectionFactor = infectionFactor
+        self.recalculationPeriod = recalculationPeriod
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func createHumans() {
+        for _ in 1...groupSize {
+            humans.append(Human(x: Int.random(in: 0...Int(simulationView.frame.width)), y: Int.random(in: 0...Int(simulationView.frame.height))))
+        }
+    }
+    
+    func drawHumans() {
+        humans.forEach { (human) in
+            let smallFrame = CGRect(x: human.x, y: human.y, width: 30, height: 30)
+            let square = UIView(frame: smallFrame)
+            square.backgroundColor = .green
+            
+            simulationView.addSubview(square)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
